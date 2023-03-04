d51b69bc8ef6786de59bd0545f5d50b04f48c0a8
dimsp98 <dspiliotis98@gmail.com>
2023-03-04 20:27:32 +0200

    added bar height patch

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const Gap default_gap        = {.isgap = 2, .realgap = 8, .gappx = 8 };
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 5;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { 
