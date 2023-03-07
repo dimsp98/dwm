191e48e298f9cff05da38cbf129e718b1a8d713c
dimsp98 <dspiliotis98@gmail.com>
2023-03-07 23:55:27 +0200

    updated

--- autostart.sh
+++ autostart.sh
@@ -6,9 +6,6 @@ xrandr --output HDMI-1 --mode 1920x1080 --rate 60 &
 xset s off &
 xset -dpms &
 
-#statusbar
-slstatus &
-
 #keybindings 
 pgrep -x sxhkd > /dev/null || sxhkd -c $HOME/.config/sxhkd/sxhkdrc &
 
@@ -27,4 +24,5 @@ setxkbmap -model pc104 -layout us,gr -option 'grp:alt_shift_toggle' &
 #notifications
 pgrep -x dunst > /dev/null || dunst -c ~/.config/dunst/dunstrc &
 
+exec /usr/local/bin/slstatus &
 exec /usr/local/bin/dwm 
